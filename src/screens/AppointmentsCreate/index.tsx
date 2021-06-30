fe39269c41fd7bd25a7fa7409f917194a170089a
Rudinei Costa <costaaugusto177@outlook.com>
2021-06-30 13:24:09 -0300

    feat: form in screen appointments create

--- src/screens/AppointmentsCreate/index.tsx
+++ src/screens/AppointmentsCreate/index.tsx
@@ -11,6 +11,7 @@ import { Header } from '../../components/Header';
 
 import {theme} from '../../global/styles/theme';
 import {styles} from './styles';
+import { SmallInput } from '../../components/SmallInput';
 
 
 export function AppointmentsCreate() {
@@ -18,7 +19,12 @@ export function AppointmentsCreate() {
   return (
     <Background>
       <Header title={'Agendar Partida'}/>
-      <Text style={styles.label}> Categoria </Text>
+      <Text style={[
+        styles.label,
+        {marginTop:32, marginLeft:24,marginBottom: 15}]
+        }>
+         Categoria 
+      </Text>
 
       <CategorySelect 
         hasChecked
@@ -28,22 +34,53 @@ export function AppointmentsCreate() {
 
       <View style={styles.form}>
         <RectButton>
-          <View style={styles.selection}>
+          <View style={styles.select}>
+
             <View style={styles.image}/>
+
             <View style={styles.selectBody}>
               <Text style={styles.label}>
                 Selecionar servidor
               </Text>
             </View>
+
             <Feather 
               name='arrow-right' 
               color={theme.colors.heading}
               size={18}
             />
+
           </View>
         </RectButton>
-      </View>
-        
+      
+        <View style={styles.field}>
+          {/* field está em flex direction row 
+            ou seja, uma view vai ficar do lado da outra
+          */}
+          <View>
+            <Text style={styles.label}> Dia e mês</Text>
+
+            <View style={styles.column}>
+              <SmallInput maxLength={2}/>
+              <Text style={styles.divider}> / </Text>
+              <SmallInput maxLength={2}/>
+            </View>
+          </View>
+
+          <View>
+            <Text style={styles.label}> Hora e minuto</Text>
+
+            <View style={styles.column}>
+              <SmallInput maxLength={2}/>
+              <Text style={styles.divider}> : </Text>
+              <SmallInput maxLength={2}/>
+            </View>
+          </View>
+
+        </View>
+      </View>  
+      
+      
       </Background>
 
   )
--- src/screens/AppointmentsCreate/styles.ts
+++ src/screens/AppointmentsCreate/styles.ts
@@ -16,14 +16,14 @@ export const styles = StyleSheet.create({
     marginTop:32,
 
   },
-  selection:{
+  select:{
     flexDirection: 'row',
     width:'100%',
     height: 68,
     alignItems: 'center',
+    borderColor:theme.colors.secondary50,
     borderWidth: 1,
     borderRadius:8,
-    borderColor:theme.colors.secondary50,
     paddingRight:25,
   },
   selectBody:{
@@ -33,14 +33,30 @@ export const styles = StyleSheet.create({
   image:{
     width: 64,
     height: 68,
-    backgroundColor:'#1D2766',
+    backgroundColor:theme.colors.secondary40,
     borderWidth: 1,
-    borderColor:'#243189',
+    borderColor:theme.colors.secondary50,
     borderRadius:8,
   },
   title:{
     fontFamily: theme.fonts.title700,
     fontSize:18,
     color: theme.colors.heading,
+  },
+  field:{
+    width: '100%',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginTop:30,
+  },
+  column:{
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  divider:{
+    marginRight:4,
+    fontSize:15,
+    fontFamily:theme.fonts.text500,
+    color:theme.colors. heading,
   }
 })
